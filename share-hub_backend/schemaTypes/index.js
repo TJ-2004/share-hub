56d5d438ba6081125feafebaf7261a5898c8bb3d
TJ-2004 <124814940+TJ-2004@users.noreply.github.com>
2024-02-28 02:15:31 +0530

    Initialze backend

---
+++ share-hub_backend/schemaTypes/index.js
@@ -0,0 +1,7 @@
+import pin from './pin.js'
+import comment from './comment.js'
+import save from './save.js'
+import user from './user.js'
+import postedBy from './postedBy.js'
+
+export const schemaTypes = [user, pin, save, postedBy, comment]
