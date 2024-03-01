658590eb3b5e8c24213ce75bc38d8abee37f5718
TJ-2004 <124814940+TJ-2004@users.noreply.github.com>
2024-03-01 17:44:48 +0530

    Complete Navbar

--- share-hub_frontend/src/container/Pins.jsx
+++ share-hub_frontend/src/container/Pins.jsx
@@ -1,11 +1,32 @@
-import React from 'react'
-
-const Pins = () => {
+import React, { useState } from "react";
+import { Routes, Route } from "react-router-dom";
+import { Navbar, Feed, CreatePin, PinDetail, Search } from "../components";
+const Pins = ({ user }) => {
+  const [SearchTerm, setSearchTerm] = useState("");
   return (
-    <div>
-      Pins
+    <div className="px-2 md:px-5 ">
+      <div className="bg-gray-50">
+        <Navbar SearchTerm={SearchTerm} setSearchTerm={setSearchTerm}  user = {user}/>
+      </div>
+      <div className="h-full">
+        <Routes>
+          <Route path="/" element={<Feed />} />
+          <Route path="/category/:categoryId" element={<Feed />} />
+          <Route
+            path="/pin-detail/:pinId"
+            element={<PinDetail user={user} />}
+          />
+          <Route path="/create-pin" element={<CreatePin user={user} />} />
+          <Route
+            path="/search"
+            element={
+              <Search SearchTerm={SearchTerm} setSearchTerm={setSearchTerm} />
+            }
+          />
+        </Routes>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Pins
+export default Pins;
